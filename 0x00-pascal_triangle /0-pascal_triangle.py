997f9af4594b78ef512656ab1dffcecfd9c1bfda
Radwa Khaled <131785821+radwakhaled204@users.noreply.github.com>
2024-11-22 16:34:15 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle /0-pascal_triangle.py
+++ 0x00-pascal_triangle /0-pascal_triangle.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
+
 """
-Pascal triangle module
+Create a function that returns a list of lists of integers
+representing the Pascal's triangles of n
 """
 
 
-def pascal_triangle(n):
-    """ returns the Pascal's Triangle of size n. """
+def pascal_triangle(n: int) -> list:
+    """
+    Returns a list of list of intergers
+    Args:
+        n (int): size of the triangles
+    Returns:
+        list
+    """
+    if not isinstance(n, int):
+        raise TypeError(f"{n} is not an integer")
     if n <= 0:
-        return []
+        return ([])
+    triangles = []
+    for i in range(n):
+        temp = []
 
-    pascalTriangle = [[1]]
-    while len(pascalTriangle) != n:
-        last_row = pascalTriangle[-1]
-        next_row = [1]
-
-        for i in range(len(last_row) - 1):
-            next_row.append(last_row[i] + last_row[i + 1])
-        next_row.append(1)
-        pascalTriangle.append(next_row)
-
-    return pascalTriangle
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp.append(1)
+            else:
+                temp.append(triangles[i-1][j-1] + triangles[i-1][j])
+        triangles.append(temp)
+    return triangles
